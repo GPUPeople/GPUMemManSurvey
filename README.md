7d7e87f1fba71beeeca20eee7aa687157aadb6a9
Martin Winter <martin.winter@icg.tugraz.at>
2020-01-31 14:10:26 +0100

    [Halloc] Setup Script for halloc

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # GPUMemManSurvey
 Evaluating different memory managers for dynamic GPU memory
 
+# Instructions
+To use the automated setup script, please install `gitpython` (Arch Linux: `pacman -S python-gitpython`, Windows: `pip install gitpython`).
+Execute the automated script!
+
 # Work in Progress!
 
 | Framework | Status |
--- SetupScript.py
+++ SetupScript.py
@@ -1,15 +1,28 @@
-from git
+from git import Repo
 import os
+from shutil import copy as copyFile
 
 def main():
 	print("------------------------")
 	print("Setup Halloc")
 	print("------------------------")
 
-	if not os.path.isdir(frameworks/halloc/repository"):
-		git.Git("frameworks/halloc/repository").clone("https://github.com/canonizer/halloc.git")
-	else
+	if not os.path.isdir("frameworks/halloc/repository"):
+		Repo.clone_from("https://github.com/canonizer/halloc.git", "frameworks/halloc/repository")
+	else:
 		print("Halloc Repository already cloned")
+
+	print("Overwrite existing files with corrected files!")
+	fixed_files_path = "frameworks/halloc/fixes/"
+	for file in os.listdir(fixed_files_path):
+		filename = fixed_files_path + os.fsdecode(file)
+		print("Copy file " + filename)
+		copyFile(filename, "frameworks/halloc/repository/src/")
+	
+	print("------------------------")
+	print("Halloc is ready to use!")
+	print("------------------------")
+
 
 
 if __name__ == "__main__":
