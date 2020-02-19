82baaa10eb9e3536543d7ecde0cae5a33e13e5e3
Martin Winter <martin.winter@icg.tugraz.at>
2020-02-19 11:26:54 +0100

    Some initial results on allocation performance

--- scripts/test_allocation.py
+++ scripts/test_allocation.py
@@ -5,39 +5,61 @@ import time
 from datetime import datetime
 from timedprocess import Command
 
+import argparse
+
 def main():
 	# Run all files from a directory
 	print("##############################################################################")
 	print("Callable as: python test_allocation.py")
 	print("##############################################################################")
 	
-	build_path = "../build/"
-	testcases = {
-		 "c_allocation",
-		 "h_allocation",
-		 "o_allocation",
-		 "s_allocation"
-	}
-
-	test_paths = list()
-	for testcase in testcases:
-		test_paths.append(build_path + testcase)
-
-	test_paths.sort()
-
-	print(test_paths)
-
 	# Specify which test configuration to use
+	testcases = list()
 	num_allocations = 10000
 	smallest_allocation_size = 4
 	largest_allocation_size = 1024
 	free_memory = 1
+	build_path = "../build/"
+
+	parser = argparse.ArgumentParser(description='Test allocation performance for various frameworks')
+	parser.add_argument('-t', type=str, help='Specify which frameworks to test, separated by +, e.g. o+s+h+c')
+	parser.add_argument('-num', type=int, help='How many allocations to perform')
+	parser.add_argument('-range', type=str, help='Sepcify Allocation Range, e.g. 4-1024')
+
+	args = parser.parse_args()
+
+	# Parse approaches
+	if(args.t):
+		selected_approaches = args.t.split('+')
+		if any("h" in s for s in args.t):
+			testcases.append(build_path + str("h_allocation"))
+		if any("s" in s for s in args.t):
+			testcases.append(build_path + str("s_allocation"))
+		if any("o" in s for s in args.t):
+			testcases.append(build_path + str("o_allocation"))
+		if any("c" in s for s in args.t):
+			testcases.append(build_path + str("c_allocation"))
+	# Parse num allocation
+	if(args.num):
+		num_allocations = args.num
+
+	# Parse range
+	if(args.range):
+		selected_range = args.range.split('-')
+		smallest_allocation_size = int(selected_range[0])
+		largest_allocation_size = int(selected_range[1])
+
+	testcases.sort()
+
+	print(testcases)
+
+	exit()
 
 	# Timeout (in seconds)
 	time_out_val = 10;
 
 	# Run executables
-	for executable in test_paths:
+	for executable in testcases:
 		smallest_allocation_size = 4
 		while smallest_allocation_size < largest_allocation_size:
 			run_config = str(num_allocations) + " " + str(smallest_allocation_size) + " 0 " + str(free_memory)
