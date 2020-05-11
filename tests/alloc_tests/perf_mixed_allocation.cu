d0322cd22bde460c18ca4c01abe71c4b91d3920b
Martin Winter <martin.winter@icg.tugraz.at>
2020-05-11 10:57:10 +0200

    Mixed testcase done

--- tests/alloc_tests/perf_mixed_allocation.cu
+++ tests/alloc_tests/perf_mixed_allocation.cu
@@ -25,6 +25,117 @@
 #include "regeff/Instance.cuh"
 #endif
 
+template <typename MemoryManagerType, bool warp_based>
+__global__ void d_testAllocation(MemoryManagerType mm, int** verification_ptr, int num_allocations, unsigned int* allocation_size)
+{
+	int tid{0};
+	if(warp_based)
+	{
+		tid = (threadIdx.x + blockIdx.x * blockDim.x) / 32;
+		if(threadIdx.x % 32 != 0)
+			return;
+	}
+	else
+	{
+		tid = threadIdx.x + blockIdx.x * blockDim.x;
+	}
+	if(tid >= num_allocations)
+		return;
+
+	verification_ptr[tid] = reinterpret_cast<int*>(mm.malloc(allocation_size[tid]));
+}
+
+template <typename MemoryManagerType>
+__global__ void d_testAllocation(MemoryManagerType mm, int** verification_ptr, int num_allocations, unsigned int* allocation_size, DevicePerfMeasure::Type* timing)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if(tid >= num_allocations)
+		return;
+	
+	DevicePerf perf_measure;
+	auto alloc_size = allocation_size[tid];
+	
+	// Start Measure
+	perf_measure.startThreadMeasure();
+
+	auto ptr = reinterpret_cast<int*>(mm.malloc(alloc_size));
+	
+	// Stop Measure
+	timing[tid] = perf_measure.stopThreadMeasure();
+
+	verification_ptr[tid] = ptr;
+}
+
+__global__ void d_testWriteToMemory(int** verification_ptr, int num_allocations, unsigned int* allocation_size)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if(tid >= num_allocations)
+		return;
+	
+	auto ptr = verification_ptr[tid];
+	auto alloc_size = allocation_size[tid];
+	for(auto i = 0; i < (alloc_size / sizeof(int)); ++i)
+	{
+		ptr[i] = tid;
+	}
+}
+
+__global__ void d_testReadFromMemory(int** verification_ptr, int num_allocations, unsigned int* allocation_size)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if(tid >= num_allocations)
+		return;
+	
+	auto ptr = verification_ptr[tid];
+	auto alloc_size = allocation_size[tid];
+	for(auto i = 0; i < (alloc_size / sizeof(int)); ++i)
+	{
+		if(ptr[i] != tid)
+		{
+			printf("%d | We got a wrong value here! %d vs %d\n", tid, ptr[i], tid);
+			__trap();
+		}
+	}
+}
+
+template <typename MemoryManagerType, bool warp_based>
+__global__ void d_testFree(MemoryManagerType mm, int** verification_ptr, int num_allocations)
+{
+	int tid{0};
+	if(warp_based)
+	{
+		tid = (threadIdx.x + blockIdx.x * blockDim.x) / 32;
+		if(threadIdx.x % 32 != 0)
+			return;
+	}
+	else
+	{
+		tid = threadIdx.x + blockIdx.x * blockDim.x;
+	}
+	if(tid >= num_allocations)
+		return;
+
+	mm.free(verification_ptr[tid]);
+}
+
+template <typename MemoryManagerType>
+__global__ void d_testFree(MemoryManagerType mm, int** verification_ptr, int num_allocations, DevicePerfMeasure::Type* timing)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	if(tid >= num_allocations)
+		return;
+	
+	DevicePerf perf_measure;
+
+	// Start Measure
+	perf_measure.startThreadMeasure();
+
+	mm.free(verification_ptr[tid]);
+
+	// Stop Measure
+	timing[tid] = perf_measure.stopThreadMeasure();
+}
+
 int main(int argc, char* argv[])
 {
 	unsigned int num_allocations{10000};
@@ -32,6 +143,7 @@ int main(int argc, char* argv[])
 	unsigned int allocation_size_byte_high{8192};
 	int num_iterations {100};
 	bool warp_based{false};
+	bool print_output{true};
 	bool onDeviceMeasure{false};
 	bool generate_output{false};
 	bool free_memory{true};
@@ -142,7 +254,7 @@ int main(int argc, char* argv[])
 
 	std::vector<unsigned int> allocation_sizes(num_allocations);
 	unsigned int* d_allocation_sizes{nullptr};
-	int* d_memory{nullptr};
+	int** d_memory{nullptr};
 	CHECK_ERROR(cudaMalloc(&d_memory, sizeof(int*) * num_allocations));
 	CHECK_ERROR(cudaMalloc(&d_allocation_sizes, sizeof(unsigned int) * num_allocations));
 
@@ -169,15 +281,91 @@ int main(int argc, char* argv[])
 	auto offset = allocation_size_byte_low;
 	for(auto i = 0; i < num_iterations; ++i)
 	{
-		std::mt19937 gen(i); //Standard mersenne_twister_engine seeded with rd()
+		std::mt19937 gen(i); //Standard mersenne_twister_engine seeded with iteration
     	std::uniform_real_distribution<> dis(0.0, 1.0);
 		// Generate sizes
 		srand(i);
 		for(auto i = 0; i < num_allocations; ++i)
-			allocation_sizes[i] = offset + dis(gen) * range;
+			allocation_sizes[i] = Utils::alignment(offset + dis(gen) * range, sizeof(int));
 		CHECK_ERROR(cudaMemcpy(d_allocation_sizes, allocation_sizes.data(), sizeof(unsigned int) * num_allocations, cudaMemcpyHostToDevice));
 
 		// TODO:: Continue
+		if(onDeviceMeasure)
+		{
+			d_testAllocation <<<gridSize, blockSize>>>(memory_manager, d_memory, num_allocations, d_allocation_sizes, per_thread_timing_allocation.getDevicePtr());
+			CHECK_ERROR(cudaDeviceSynchronize());
+			per_thread_timing_allocation.acceptResultsFromDevice();
+		}
+		else
+		{
+			timing_allocation.startMeasurement();
+			if(warp_based)
+				d_testAllocation <decltype(memory_manager), true> <<<gridSize, blockSize>>>(memory_manager, d_memory, num_allocations, d_allocation_sizes);
+			else
+				d_testAllocation <decltype(memory_manager), false> <<<gridSize, blockSize>>>(memory_manager, d_memory, num_allocations, d_allocation_sizes);
+			timing_allocation.stopMeasurement();
+			CHECK_ERROR(cudaDeviceSynchronize());
+		}
+
+		d_testWriteToMemory<<<gridSize, blockSize>>>(d_memory, num_allocations, d_allocation_sizes);
+
+		CHECK_ERROR(cudaDeviceSynchronize());
+
+		d_testReadFromMemory<<<gridSize, blockSize>>>(d_memory, num_allocations, d_allocation_sizes);
+
+		CHECK_ERROR(cudaDeviceSynchronize());
+
+		if(free_memory)
+		{
+			if(onDeviceMeasure)
+			{
+				d_testFree <<<gridSize, blockSize>>>(memory_manager, d_memory, num_allocations, per_thread_timing_free.getDevicePtr());
+				CHECK_ERROR(cudaDeviceSynchronize());
+				per_thread_timing_free.acceptResultsFromDevice();
+			}
+			else
+			{
+				timing_free.startMeasurement();
+				if(warp_based)
+					d_testFree <decltype(memory_manager), true> <<<gridSize, blockSize>>>(memory_manager, d_memory, num_allocations);
+				else
+					d_testFree <decltype(memory_manager), false> <<<gridSize, blockSize>>>(memory_manager, d_memory, num_allocations);
+				timing_free.stopMeasurement();
+				CHECK_ERROR(cudaDeviceSynchronize());
+			}
+		}
+	}
+
+	if(onDeviceMeasure)
+	{
+		auto alloc_result = per_thread_timing_allocation.generateResult();
+		auto free_result = per_thread_timing_free.generateResult();
+
+		if(print_output)
+		{
+			std::cout << "Timing Allocation: Mean:" << alloc_result.mean_ << "cycles | Median: " << alloc_result.median_ << " ms" << std::endl;
+			std::cout << "Timing       Free: Mean:" << free_result.mean_ << "cycles | Median: " << free_result.median_ << " ms" << std::endl;
+		}
+		if(generate_output)
+		{
+			results_alloc << alloc_result.mean_ << "," << alloc_result.std_dev_ << "," << alloc_result.min_ << "," << alloc_result.max_ << "," << alloc_result.median_;
+			results_free << free_result.mean_ << "," << free_result.std_dev_ << "," << free_result.min_ << "," << free_result.max_ << "," << free_result.median_;
+		}
+	}
+	else
+	{
+		auto alloc_result = timing_allocation.generateResult();
+		auto free_result = timing_free.generateResult();
+		if(print_output)
+		{
+			std::cout << "Timing Allocation: Mean:" << alloc_result.mean_ << "ms" << std::endl;// " | Median: " << alloc_result.median_ << " ms" << std::endl;
+			std::cout << "Timing       Free: Mean:" << free_result.mean_ << "ms" << std::endl;// "  | Median: " << free_result.median_ << " ms" << std::endl;
+		}
+		if(generate_output)
+		{
+			results_alloc << alloc_result.mean_ << "," << alloc_result.std_dev_ << "," << alloc_result.min_ << "," << alloc_result.max_ << "," << alloc_result.median_;
+			results_free << free_result.mean_ << "," << free_result.std_dev_ << "," << alloc_result.min_ << "," << alloc_result.max_ << "," << free_result.median_;
+		}
 	}
 
 	CHECK_ERROR(cudaFree(d_allocation_sizes));
