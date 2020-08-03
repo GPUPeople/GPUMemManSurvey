92041394b884d30004a6f4a740ac6d1bb02f80cc
Martin Winter <martin.winter@icg.tugraz.at>
2020-08-03 12:00:05 +0200

    Graph tests, many not working

--- tests/graph_tests/test_graph_init.py
+++ tests/graph_tests/test_graph_init.py
@@ -27,10 +27,10 @@ graphs = [
 	"vsp_mod2_pgp2_slptsk.mtx"
 ]
 
-graphs = [
-	"adaptive.mtx"
-	# "hugetric-00000.mtx"
-]
+# graphs = [
+# 	"adaptive.mtx"
+# 	"hugetric-00000.mtx"
+# ]
 
 path = "../../../graphs/"
 
@@ -82,10 +82,10 @@ def main():
 		if any("f" in s for s in args.t):
 			testcases["FDGMalloc"] = sync_build_path + str("f_graph_test")
 		if any("r" in s for s in args.t):
-			# testcases["RegEff-A"] = sync_build_path + str("r_graph_test_a")
+			testcases["RegEff-A"] = sync_build_path + str("r_graph_test_a")
 			# testcases["RegEff-AW"] = sync_build_path + str("r_graph_test_aw")
-			testcases["RegEff-C"] = sync_build_path + str("r_graph_test_c")
-			testcases["RegEff-CF"] = sync_build_path + str("r_graph_test_cf")
+			# testcases["RegEff-C"] = sync_build_path + str("r_graph_test_c")
+			# testcases["RegEff-CF"] = sync_build_path + str("r_graph_test_cf")
 			# testcases["RegEff-CM"] = sync_build_path + str("r_graph_test_cm")
 			# testcases["RegEff-CFM"] = sync_build_path + str("r_graph_test_cfm")
 	
