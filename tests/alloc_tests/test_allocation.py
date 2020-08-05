f0ce25f1b62cdcf3518651dca417002a2bf95f03
Martin Winter <martin.winter@icg.tugraz.at>
2020-08-05 11:07:37 +0200

    Partial warp-based results

--- tests/alloc_tests/test_allocation.py
+++ tests/alloc_tests/test_allocation.py
@@ -66,11 +66,11 @@ def main():
 			testcases["FDGMalloc"] = sync_build_path + str("f_alloc_test")
 		if any("r" in s for s in args.t):
 			# testcases["RegEff-A"] = sync_build_path + str("r_alloc_test_a")
-			testcases["RegEff-AW"] = sync_build_path + str("r_alloc_test_aw")
-			testcases["RegEff-C"] = sync_build_path + str("r_alloc_test_c")
+			# testcases["RegEff-AW"] = sync_build_path + str("r_alloc_test_aw")
+			# testcases["RegEff-C"] = sync_build_path + str("r_alloc_test_c")
 			testcases["RegEff-CF"] = sync_build_path + str("r_alloc_test_cf")
-			testcases["RegEff-CM"] = sync_build_path + str("r_alloc_test_cm")
-			testcases["RegEff-CFM"] = sync_build_path + str("r_alloc_test_cfm")
+			# testcases["RegEff-CM"] = sync_build_path + str("r_alloc_test_cm")
+			# testcases["RegEff-CFM"] = sync_build_path + str("r_alloc_test_cfm")
 	
 	# Parse num allocation
 	if(args.num):
