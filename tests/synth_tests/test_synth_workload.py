9029d546922180d25d5704edcae9a5b7616ec901
Testy the Testserver <testserver@testserver.at>
2020-08-12 10:19:39 +0200

    Write tests

--- tests/synth_tests/test_synth_workload.py
+++ tests/synth_tests/test_synth_workload.py
@@ -74,7 +74,7 @@ def main():
 			# testcases["RegEff-A"] = sync_build_path + str("r_synth_test_a")
 			# testcases["RegEff-AW"] = sync_build_path + str("r_synth_test_aw")
 			# testcases["RegEff-C"] = sync_build_path + str("r_synth_test_c")
-			testcases["RegEff-CF"] = sync_build_path + str("r_synth_test_cf")
+			# testcases["RegEff-CF"] = sync_build_path + str("r_synth_test_cf")
 			testcases["RegEff-CM"] = sync_build_path + str("r_synth_test_cm")
 			testcases["RegEff-CFM"] = sync_build_path + str("r_synth_test_cfm")
 		if any("b" in s for s in args.t):
