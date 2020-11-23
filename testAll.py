cb8e95a7c8c23b4f2491e2692f0643bfd34a2e43
Martin Winter <martin.winter@icg.tugraz.at>
2020-11-23 12:20:37 +0100

    CMake update

--- testAll.py
+++ testAll.py
@@ -13,45 +13,41 @@ def main():
 	current_dir = os.getcwd()
 
 	print("The selected amount of manageable memory is: {} Gb".format(str(args.mem_size)))
-	print("Beware that the graph tests set their size in 'tests/graph_tests/config*.json', do you want to continue? (y/n)")
-	inputfromconsole = input()
-	if not (inputfromconsole == "yes" or inputfromconsole == "y"):
-		exit()
 
 	# Which tests to run
 	tests = {
 		"alloc_tests" : [
-			"python test_allocation.py -t o+s+h+c+r+x -num 10000 -range 4-64 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)),
-			"python test_mixed_allocation.py -t o+s+h+c+r+x -num 10000 -range 4-64 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)),
-			"python test_scaling.py -t o+s+h+c+r+x -byterange 4-64 -threadrange 0-10 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size))
+			["python test_allocation.py -t o+s+h+c+r+x -num 10000 -range 4-32 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)), "performance"],
+			# ["python test_mixed_allocation.py -t o+s+h+c+r+x -num 10000 -range 4-32 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)), "mixed_performance"],
+			# ["python test_scaling.py -t o+s+h+c+r+x -byterange 4-32 -threadrange 0-10 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)), "scaling"]
 		],
-		"frag_tests"  : [
-			"python test_fragmentation.py -t o+s+h+c+r+x -num 10000 -range 4-64 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)),
-			"python test_oom.py -t o+s+h+c+r+x -num 10000 -range 4-64 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size))
-		],
-		"graph_tests" : [
-			"python test_graph_init.py -t o+s+h+c+r+x -configfile config_init.json -runtest -genres -timeout 120",
-			"python test_graph_update.py -t o+s+h+c+r+x -configfile config_update.json -runtest -genres -timeout 120",
-			"python test_graph_update.py -t o+s+h+c+r+x -configfile config_update_range.json -runtest -genres -timeout 120"
-		],
-		"synth_tests" : [
-			"python test_registers.py -t o+s+h+c+r+x -runtest -genres -allocsize {}".format(str(args.mem_size)),
-			"python test_synth_init.py -t o+s+h+c+r+x -runtest -genres -allocsize {}".format(str(args.mem_size)),
-			"python test_synth_workload.py -t o+s+h+c+r+x -threadrange 0-10 -range 4-64 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)),
-			"python test_synth_workload.py -t o+s+h+c+r+x -threadrange 0-10 -range 4-64 -iter 50 -runtest -genres -testwrite -timeout 60 -allocsize {}".format(str(args.mem_size))
-		]
+		# "frag_tests"  : [
+		# 	["python test_fragmentation.py -t o+s+h+c+r+x -num 10000 -range 4-16 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)), ""],
+		# 	["python test_oom.py -t o+s+h+c+r+x -num 10000 -range 4-16 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)), ""]
+		# ],
+		# "graph_tests" : [
+		# 	["python test_graph_init.py -t o+s+h+c+r+x -configfile config_init.json -runtest -genres -timeout 120", ""],
+		# 	["python test_graph_update.py -t o+s+h+c+r+x -configfile config_update.json -runtest -genres -timeout 120", ""],
+		# 	["python test_graph_update.py -t o+s+h+c+r+x -configfile config_update_range.json -runtest -genres -timeout 120", ""]
+		# ],
+		# "synth_tests" : [
+		# 	["python test_registers.py -t o+s+h+c+r+x -runtest -genres -allocsize {}".format(str(args.mem_size)), ""],
+		# 	["python test_synth_init.py -t o+s+h+c+r+x -runtest -genres -allocsize {}".format(str(args.mem_size)), ""],
+		# 	["python test_synth_workload.py -t o+s+h+c+r+x -threadrange 0-10 -range 4-32 -iter 50 -runtest -genres -timeout 60 -allocsize {}".format(str(args.mem_size)), ""],
+		# 	["python test_synth_workload.py -t o+s+h+c+r+x -threadrange 0-10 -range 4-32 -iter 5 -runtest -genres -testwrite -timeout 60 -allocsize {}".format(str(args.mem_size)), ""]
+		# ]
 	}
 
 	for path, commands in tests.items():
 		for command in commands:
-			print("Will execute command in folder {0}: {1}".format(path, command))
+			print("Will execute command in folder {0} (subfolder {2}): {1}".format(path, command[0], command[1]))
 
-	# # Run tests	
-	# for path, commands in tests.items():
-	# 	for command in commands:
-	# 		os.chdir(os.path.join(current_dir, "tests", path))
-	# 		Command(command).run(timeout=3600)
-	# 		shutil.move("tests/{}/results/aggregate/*".format(path), "results")
+	# Run tests	
+	for path, commands in tests.items():
+		for command in commands:
+			os.chdir(os.path.join(current_dir, "tests", path))
+			Command(command[0]).run(timeout=3600)
+			shutil.move("tests/{0}/results/{1}aggregate/*".format(path, command[1] + "/"), "results")
 
 if __name__ == "__main__":
 	main()
